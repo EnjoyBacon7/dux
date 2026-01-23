0101e3d52863d2ae1b00d7d2053b659ede7dd03a
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2026-01-23 14:51:12 +0100

    handle minification size warning

--- dux-front/vite.config.ts
+++ dux-front/vite.config.ts
@@ -5,4 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/static/',
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'markdown': ['react-markdown'],
+        },
+      },
+    },
+  },
 })
